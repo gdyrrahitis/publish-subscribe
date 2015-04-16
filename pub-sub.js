d07eec5351ebc54daf4b8fd4e796fcde71e90fe6
unknown <g.dyrrahitis@RDC-57.sieben.gr>
2015-04-16 17:03:33 +0300

    first commit

---
+++ pub-sub.js
@@ -0,0 +1,12 @@
+(function ($) {
+    var obj = $({});
+    $.each({
+        trigger: 'publish',
+        on: 'subscribe',
+        off: 'unsubscribe'
+    }, function (key, value) {
+        $[value] = function () {
+            obj[key].apply(obj, arguments);
+        };
+    });
+})(jQuery, undefined);
